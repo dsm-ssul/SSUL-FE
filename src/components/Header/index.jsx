f4046f108295a8e26729d95cf46fba280ca3ea3b
taeyoooon <leteyune101010@gmail.com>
2024-07-17 13:38:52 +0900

    header nav 설정

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,19 @@
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/Logo.svg";
 import * as S from "./style.js";
 
-// const getNavLinkStyle = ({ isActive }) => ({
-//   color: isActive ? "#4F63D2" : "black",
-// });
+const getNavLinkStyle = ({ isActive }) => ({
+  color: isActive ? "#4F63D2" : "black",
+});
 
 function Header() {
   return (
     <S.Container>
       <Logo />
-      <S.Text>재무설계</S.Text>
-      <S.Text>재무투표</S.Text>
-      <S.Text>지식공유</S.Text>
-      <S.Text>계산기</S.Text>
-      {/* <NavLink to="/main" style={getNavLinkStyle}>
+      <NavLink to="/main" style={getNavLinkStyle}>
         <S.Text>재무설계</S.Text>
       </NavLink>
-      <NavLink to="/vote" style={getNavLinkStyle}>
+      <NavLink to="/voting" style={getNavLinkStyle}>
         <S.Text>재무투표</S.Text>
       </NavLink>
       <NavLink to="/share" style={getNavLinkStyle}>
@@ -25,7 +21,7 @@ function Header() {
       </NavLink>
       <NavLink to="/calculator" style={getNavLinkStyle}>
         <S.Text>계산기</S.Text>
-      </NavLink> */}
+      </NavLink>
     </S.Container>
   );
 }
